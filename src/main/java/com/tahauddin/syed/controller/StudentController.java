a25e0b083167b2f7575c48591c219b4115acfc58
Tahauddin-Syed <tahauddin.syed@gmail.com>
2022-08-13 10:44:07 +0300

    Added One PostMapping Method

--- src/main/java/com/tahauddin/syed/controller/StudentController.java
+++ src/main/java/com/tahauddin/syed/controller/StudentController.java
@@ -1,17 +1,15 @@
 package com.tahauddin.syed.controller;
 
 import com.tahauddin.syed.dto.StudentDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping("/api/v1/student")
+@Slf4j
 public class StudentController {
-
 
     @GetMapping("/{id}")
     public Mono<ResponseEntity<StudentDto>> getStudent(@PathVariable("id") Long id){
@@ -19,7 +17,9 @@ public class StudentController {
         return Mono.just(ResponseEntity.ok(studentDto));
     }
 
-
-
-
+    @PostMapping("/add")
+    public Mono<ResponseEntity<Void>> addStudent(){
+        log.info("add() Called...");
+        return Mono.just(ResponseEntity.ok().build());
+    }
 }
